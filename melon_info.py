7549469cb0016a1ce2df7fca94a7592107c54a22
Caitlin Wright <misscaitlinawright@gmail.com>
2020-08-01 02:01:31 +0000

    created pseudocode of plan and commented-out original function

--- melon_info.py
+++ melon_info.py
@@ -1,7 +1,10 @@
 """Print out all the melons in our inventory."""
 
 # PLAN
-# create for loop to loop through keys in the 3 dictionaries in melons.py
+# adjust melons.py to be a "melons" dictionary of each melon (key: melon; values: price, etc.)
+# import melons function from melons document
+# create new function with parameter melons (document)
+# create for loop to print key and values in "melons" dictionary
 
 # Ideal Output:
 # CASABA
@@ -12,20 +15,28 @@
 #     rind_color: None
 
 
-from melons import melon_names, melon_seedlessness, melon_prices
 
 
-def print_melon(name, seedless, price):
-    """Print each melon with corresponding attribute information."""
-
-    have_or_have_not = 'have'
-    if seedless:
-        have_or_have_not = 'do not have'
-
-    print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
 
 
-for i in melon_names:
-    print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
 
-print(print_melon)
+
+
+
+# from melons import melon_names, melon_seedlessness, melon_prices
+
+
+# def print_melon(name, seedless, price):
+#     """Print each melon with corresponding attribute information."""
+
+#     have_or_have_not = 'have'
+#     if seedless:
+#         have_or_have_not = 'do not have'
+
+#     print(f'{name}s {have_or_have_not} seeds and are ${price:.2f}')
+
+
+# for i in melon_names:
+#     print_melon(melon_names[i], melon_seedlessness[i], melon_prices[i])
+
+# print(print_melon)
